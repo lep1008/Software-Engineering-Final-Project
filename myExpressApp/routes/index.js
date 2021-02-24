8f03e6454caddfbdedb42d53ff54676d223caca4
Luke Pedersen <lep1008@sru.edu>
2021-02-24 15:20:30 -0500

    post data to database working - lp - 2/24

--- myExpressApp/routes/index.js
+++ myExpressApp/routes/index.js
@@ -1,5 +1,35 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require('mysql');
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "fitfriends488",
+  database: "FitFriends"
+});
+
+router.post('/submitform',function(req,res,next) {
+console.log(req.body.name);
+console.log(req.body.user);
+console.log(req.body.pass);
+
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "INSERT INTO User VALUES ('" + req.body.user + "','" + req.body.pass + "','" + req.body.name + "')";  
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+});
+
+
+});
+
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,6 +43,19 @@ router.get("/signup", (req, res) => {
 router.get("/feed", (req, res) => {
   res.render("feed");
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
