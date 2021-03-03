58b31d45b0855a56b91a27f0ced61e2f612552fa
Benny Strawser <strawserbr@yahoo.com>
2021-03-03 15:59:08 -0500

    3-3 bs changes to database.js

--- myExpressApp/public/javascripts/database.js
+++ myExpressApp/public/javascripts/database.js
@@ -1,21 +1,28 @@
 const mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "fitfriends488",
-  database: "FitFriends"
-});
-var username ='lep';
-var name='luke';
-var pass="what";
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  var sql = "INSERT INTO User VALUES ('" + username + "','" + pass + "','" + name + "')";  
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("1 record inserted");
+  //Method to Create Database Connection
+  var con = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "fitfriends488",
+    database: "FitFriends"
   });
-});
 
+  //Variable Declaration
+  var username ='lep';
+  var name='luke';
+  var pass="what";
+
+  //Method to insert data into database
+  con.connect(function(err)
+  {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "INSERT INTO User VALUES ('" + username + "','" + pass + "','" + name + "')";  
+    con.query(sql, function (err, result)
+    {
+      if (err) throw err;
+      console.log("1 record inserted");
+    });
+  });
