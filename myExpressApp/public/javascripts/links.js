d458b67bcce83e21a0369c72975a092210813432
Benny Strawser <strawserbr@yahoo.com>
2021-03-03 15:54:58 -0500

    3-3 bs prettied up links.js

--- myExpressApp/public/javascripts/links.js
+++ myExpressApp/public/javascripts/links.js
@@ -1,15 +1,16 @@
+//signUp Method
 function signUp()
 {
     location.href='/signup';
-
 }
 
+//feed Method
 function feed()
 {
     location.href='/feed';
-
 }
 
+//index Method
 function index() 
 {
     location.href='/';
